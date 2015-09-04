7c656b568b7d2dbc3bec710c576958bd6a4daeed
Chris Keyser <chris@auth0.com>
2015-09-04 09:12:50 -0400

    Update auth0-variables.js

--- example/api-gateway/auth0-variables.js
+++ example/api-gateway/auth0-variables.js
@@ -1,3 +1,3 @@
-var AUTH0_CLIENT_ID='zKRCne6bQ8L1sGqfvRfOvBJYBCj1XCYC'; 
-var AUTH0_DOMAIN='chriskeyser.auth0.com'; 
+var AUTH0_CLIENT_ID='BUIJSW9x60sIHBw8Kd9EmCbj8eDIFxDC';
 var AUTH0_CALLBACK_URL=location.href;
+var AUTH0_DOMAIN='samples.auth0.com';
