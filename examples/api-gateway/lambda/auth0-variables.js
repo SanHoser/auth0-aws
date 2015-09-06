6ec647864a508abfcad999c35ec2d23546edfadb
Chris Keyser <chris@auth0.com>
2015-09-06 11:41:02 -0400

    removed unused variables

--- examples/api-gateway/lambda/auth0-variables.js
+++ examples/api-gateway/lambda/auth0-variables.js
@@ -1,10 +1,4 @@
 var env={};
-env.AUTH0_CLIENT_ID='BUIJSW9x60sIHBw8Kd9EmCbj8eDIFxDC'
 env.AUTH0_SECRET='ADD-YOUR-SECRET';
-env.AUTH0_CALLBACK_URL=location.href;
-env.AUTH0_ISSUER='ADD-YOUR-ISSUER';
-
-
-
 module.exports = env;
 
